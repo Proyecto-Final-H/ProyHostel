c0c4d15298bcb1485503e032fdeda237da843375
clipablo <clipablo@gmail.com>
2021-09-13 15:15:44 -0300

    issue #9 ABM Caja Clase principal

---
+++ module-simple/src/main/java/domainapp/modules/simple/caja/Caja.java
@@ -0,0 +1,149 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package domainapp.modules.simple.caja;
+
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.VersionStrategy;
+
+import com.google.common.collect.ComparisonChain;
+
+import org.apache.isis.applib.annotation.Action;
+import org.apache.isis.applib.annotation.Auditing;
+import org.apache.isis.applib.annotation.DomainObject;
+import org.apache.isis.applib.annotation.DomainObjectLayout;
+import org.apache.isis.applib.annotation.Editing;
+import org.apache.isis.applib.annotation.Parameter;
+import org.apache.isis.applib.annotation.ParameterLayout;
+import org.apache.isis.applib.annotation.Property;
+import org.apache.isis.applib.annotation.Publishing;
+import org.apache.isis.applib.annotation.Title;
+import org.apache.isis.applib.services.i18n.TranslatableString;
+import org.apache.isis.applib.services.message.MessageService;
+import org.apache.isis.applib.services.repository.RepositoryService;
+import org.apache.isis.applib.services.title.TitleService;
+
+import lombok.AccessLevel;
+import static org.apache.isis.applib.annotation.CommandReification.ENABLED;
+import static org.apache.isis.applib.annotation.SemanticsOf.IDEMPOTENT;
+import static org.apache.isis.applib.annotation.SemanticsOf.NON_IDEMPOTENT_ARE_YOU_SURE;
+
+@javax.jdo.annotations.PersistenceCapable(identityType=IdentityType.DATASTORE, schema = "simple")
+@javax.jdo.annotations.DatastoreIdentity(strategy=javax.jdo.annotations.IdGeneratorStrategy.IDENTITY, column="id")
+@javax.jdo.annotations.Version(strategy= VersionStrategy.DATE_TIME, column="version")
+@javax.jdo.annotations.Unique(name="Caja_name_UNQ", members = {"name"})
+@DomainObject(auditing = Auditing.ENABLED)
+@DomainObjectLayout()  // causes UI events to be triggered
+@lombok.Getter @lombok.Setter
+@lombok.RequiredArgsConstructor
+public class Caja implements Comparable<Caja> {
+
+    @javax.jdo.annotations.Column(allowsNull = "false", length = 40)
+    @lombok.NonNull
+    @Property()// editing disabled by default, see isis.properties
+    @Title(prepend = "Caja: ")
+    private String name;
+    
+    
+    @javax.jdo.annotations.Column(allowsNull = "true", length = 4000)
+    @Property(editing = Editing.ENABLED)
+    @Title(prepend = "Detalle de Pedido: ")
+    private String notes;
+
+//    @javax.jdo.annotations.Column(allowsNull = "true", length = 40)
+//    @lombok.NonNull
+//    @Property()
+//    @Title(prepend = "Monto: ")
+//    private Integer monto;
+//        
+    @javax.jdo.annotations.Column(allowsNull = "true")
+    @lombok.NonNull
+    @Property()
+    private Integer numerofactura;
+    
+ //   @javax.jdo.annotations.Column(allowsNull = "true", length = 40)
+ //   @lombok.NonNull
+ //   @Property()
+  //  @Title(prepend = "Tipodegasto: ")
+  //  private Tipodepago Tipodepago;
+    
+    @Action(semantics = IDEMPOTENT, command = ENABLED, publishing = Publishing.ENABLED, associateWith = "name")
+    public Caja updateName(
+            @Parameter(maxLength = 40)
+            @ParameterLayout(named = "Nombre")
+            final String name,
+   //         @ParameterLayout(named = "Monto")
+//            final Integer monto,
+            @ParameterLayout(named = "Numerofactura") 
+            final Integer numerofactura
+  //         @Parameter(maxLength = 40)
+	//	    @ParameterLayout(named = "Tipodepago")
+	//	    final String Tipodepago
+		    ) {
+        setName(name);
+      //  setMonto(monto);
+        setNumerofactura(numerofactura);
+    //    setTipodegasto(tipodegasto);
+        return this;
+    }
+
+    public String default0UpdateName() {
+        return getName();
+    }
+
+    public TranslatableString validate0UpdateName(final String name) {
+        return name != null && name.contains("!") ? TranslatableString.tr("Exclamation mark is not allowed") : null;
+    }
+
+
+    @Action(semantics = NON_IDEMPOTENT_ARE_YOU_SURE)
+    public void delete() {
+        final String title = titleService.titleOf(this);
+        messageService.informUser(String.format("'%s' deleted", title));
+        repositoryService.remove(this);
+    }
+
+
+    @Override
+    public String toString() {
+        return getName();
+    }
+
+    public int compareTo(final Caja other) {
+        return ComparisonChain.start()
+                .compare(this.getName(), other.getName())
+                .result();
+    }
+
+
+    @javax.inject.Inject
+    @javax.jdo.annotations.NotPersistent
+    @lombok.Getter(AccessLevel.NONE) @lombok.Setter(AccessLevel.NONE)
+    RepositoryService repositoryService;
+
+    @javax.inject.Inject
+    @javax.jdo.annotations.NotPersistent
+    @lombok.Getter(AccessLevel.NONE) @lombok.Setter(AccessLevel.NONE)
+    TitleService titleService;
+
+    @javax.inject.Inject
+    @javax.jdo.annotations.NotPersistent
+    @lombok.Getter(AccessLevel.NONE) @lombok.Setter(AccessLevel.NONE)
+    MessageService messageService;
+
+}
