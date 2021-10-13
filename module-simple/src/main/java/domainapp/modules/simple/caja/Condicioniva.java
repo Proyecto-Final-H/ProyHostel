af98c7ea49c0936e465cff3a4b4ba884946cbaa2
clipablo <clipablo@gmail.com>
2021-10-13 00:02:03 -0300

    issue #14 Detalles complementarios de Caja Condicion IVA

---
+++ module-simple/src/main/java/domainapp/modules/simple/caja/Condicioniva.java
@@ -0,0 +1,9 @@
+package domainapp.modules.simple.caja;
+
+public enum Condicioniva {
+
+    IVA_Responsable_Inscripto, IVA_Sujeto_Exento, Consumidor_Final, Responsable_Monotributo,
+    Sujeto_No_Categorizado,Proveedor_del_Exterior,Cliente_del_Exterior,IVA_Liberado_Ley_N_19640,
+    Monotributista_Social
+}
+
