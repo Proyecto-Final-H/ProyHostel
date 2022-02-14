d2459c0ed2c25d3c4e1be0e2ac37c576b82ffabd
clipablo <clipablo@gmail.com>
2022-02-14 00:32:18 -0300

    issue #12 Reporte de Caja

--- module-simple/src/main/java/domainapp/modules/simple/reportes/CajaRepo.java
+++ module-simple/src/main/java/domainapp/modules/simple/reportes/CajaRepo.java
@@ -1,11 +1,31 @@
 package domainapp.modules.simple.reportes;
 
 public class CajaRepo {
-    private String name;
-    public CajaRepo(String name,String condicionvent,String condicioniva){
+    public String name;
+    public Integer importe;
+    public String fecha;
+    public String numerofactura;
+    public String condicionvent;
+    public String condicioniva;
+
+    public CajaRepo(String name,Integer importe, String fecha,  String numerofactura,String condicioniva, String condicionvent)
+    {
       this.name = name;
+      this.importe = importe;
+      this.fecha = fecha;
+      this.numerofactura = numerofactura;
+      this.condicionvent = condicionvent;
+      this.condicioniva = condicioniva;
+
     }
 
-    public CajaRepo(){}
-        public String getName(){return  this.name;}
+    public CajaRepo() {
+    }
+    public String getName(){return  this.name;}
+    public String getNumerofactura(){return this.numerofactura;}
+    public Integer getimporte(){return  this.importe;}
+    public String getFecha () { return this.fecha; }
+    public String getCondicionvent(){return  this.condicionvent;}
+    public String getCondicioniva(){return  this.condicioniva;}
+
 }
